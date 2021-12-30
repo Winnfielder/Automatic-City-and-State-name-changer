1d0265d8e55d9d25f0437d95018ab68955563467
Kaboto1 <kaboto1@protonmail.com>
2021-12-30 11:13:50 +0100

    Basic structure from base mod, script to deploy, initial database for states

---
+++ deploy_local.py
@@ -0,0 +1,32 @@
+import os, shutil
+from zipfile import ZipFile
+
+root_mod_dir = os.path.expanduser("~/.local/share/Paradox Interactive/Hearts of Iron IV/mod/")
+mod_zip = os.path.join(root_mod_dir, "TWR_Localized_State_and_City_names")
+
+if os.path.exists(mod_zip):
+    os.remove(mod_zip)
+
+shutil.make_archive(mod_zip, 'zip', root_dir=os.getcwd(),base_dir="common")
+zipObj = ZipFile(mod_zip+".zip", 'a')
+zipObj.write("descriptor.mod")
+
+zipObj.close()
+
+# TODO create .mod file in root by copying descriptor and adding dynamically path
+
+mod_file_dist = os.path.join(root_mod_dir, "TWR_Localized_State_and_City_names.mod")
+if os.path.exists(mod_file_dist):
+    os.remove(mod_file_dist)
+shutil.copyfile("descriptor.mod", mod_file_dist)
+
+exp = 1  # the line where text need to be added or exp that calculates it for ex %2
+
+with open(mod_file_dist, 'r') as f:
+    lines = f.readlines()
+
+with open(mod_file_dist, 'w', encoding='utf8') as f:
+    for i, line in enumerate(lines):
+        if i == exp:
+            f.write('archive="{}.zip"\n'.format(mod_zip))
+        f.write(line)
---
+++ descriptor.mod
@@ -0,0 +1,11 @@
+name="TWR Localized State and City names"
+tags={
+	"Map"
+	"Translation"
+	"Utilities"
+}
+dependencies = {
+	"Thousand Week Reich"
+}
+version="v1"
+supported_version="1.11.4"
