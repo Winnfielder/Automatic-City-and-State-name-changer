2233e3a875a0c5a60b978c8466737161a6874a9c
Kaboto1 <kaboto1@protonmail.com>
2022-01-07 10:41:08 +0100

    Improved wiki_translate, more language + manual suggestion

--- wiki_translate.py
+++ wiki_translate.py
@@ -7,16 +7,21 @@ from tqdm import tqdm
 import pinyin # transliterate chinese
 from transliterate import translit
 
-provinces = pd.read_csv("provinces.csv", index_col=0)
-states = pd.read_csv("states.csv",index_col=0)
-wiki_wiki = wikipediaapi.Wikipedia('en')
+provinces = pd.read_csv("new provinces.csv", index_col=0)
+states = pd.read_csv("new_states.csv",index_col=0)
+reference_language = 'english'
+reference_language_code = 'en'
+manual_input = False
+wiki_wiki = wikipediaapi.Wikipedia(reference_language_code)
+
+
 
 def translate_dataframe(dataframe,language,language_code):
     for index,row in tqdm(dataframe.iterrows(),total=dataframe.shape[0]):
-        if (not pd.isnull(row['english'])):
-            english_name = row['english']
+        if (not pd.isnull(row[reference_language])):
+            reference_name = row[reference_language]
             if (pd.isnull(row[language])):
-                page_py = wiki_wiki.page(english_name)
+                page_py = wiki_wiki.page(reference_name)
                 if (language_code in page_py.langlinks):
                     page_py_it = page_py.langlinks[language_code]
                     new_name = page_py_it.title
@@ -30,37 +35,41 @@ def translate_dataframe(dataframe,language,language_code):
                     # removes comma
                     if new_name.find(',')!=-1:
                         new_name = new_name[:new_name.find(',')-1]
-                    print("{} -> {}".format(english_name,new_name))
+                    print("{} -> {}".format(reference_name,new_name))
                     dataframe.loc[index,language] = new_name
+                elif manual_input:
+                    res = input("cant find {} in {}, what is it?".format(reference_name,language))
+                    dataframe.loc[index,language] = res
     return dataframe
 
 languages = [
-#    ('chinese','zh'),
-#    ('spanish','es'),
-#    ('portuguese','pt'),
+ #   ('english','en'),
+    ('chinese','zh'),
+    ('spanish','es'),
+    ('portuguese','pt'),
     ('russian','ru'),
-#    ('dutch','nl')
-#    ('albanian','sq'),
-#    ('belarusian','be'),
-#    ('czech','cs'),
-#    ('estonian','et'),
-#    ('finnish','fi'),
-#    ('french','fr'),
+    ('dutch','nl'),
+    ('italian','it'),
+    ('albanian','sq'),
+    ('czech','cs'),
+    ('estonian','et'),
+    ('finnish','fi'),
+    ('french','fr'),
 #    ('german','de'),
    ('greek','el'),
-#    ('hungarian','hu'),
-#    ('latvian','lv'),
-#    ('lithuanian','lt'),
-#    ('polish','pl'),
-#    ('romanian','ro'),
-#    ('swedish','sv'),
- #   ('turkish','tr'),
-#    ('yugoslav','hr'),
+    ('hungarian','hu'),
+    ('latvian','lv'),
+    ('lithuanian','lt'),
+    ('polish','pl'),
+    ('romanian','ro'),
+    ('swedish','sv'),
+   ('turkish','tr'),
+    ('yugoslav','hr'),
 ]
 
 
 for language, language_code in languages:
     provinces_wiki = translate_dataframe(provinces,language,language_code)
-    provinces_wiki.to_csv("provinces.csv")
-    states_wiki = translate_dataframe(states,language,language_code)
-    states_wiki.to_csv("states.csv")
+    provinces_wiki.to_csv("new provinces.csv")
+   # states_wiki = translate_dataframe(states,language,language_code)
+   # states_wiki.to_csv("new_states.csv")
