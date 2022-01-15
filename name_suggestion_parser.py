cf63044f35cd129f2e6a5ae154d2e6011884dc46
Kaboto1 <kaboto1@protonmail.com>
2022-01-15 19:10:01 +0100

    uncommented state code and some fix in name suggestion parser

--- name_suggestion_parser.py
+++ name_suggestion_parser.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 
-filename = "translation_ideas_script/german_name.txt"
-language = "german"
+filename = "../Dynamic States and Cities Rename/french_name.txt"
+language = "french"
 
-provinces = pd.read_csv("new provinces.csv", index_col=0)
+provinces = pd.read_csv("new_provinces.csv", index_col=0)
 states = pd.read_csv("new_states.csv", index_col=0)
 
 starting_line = 0
@@ -28,26 +28,26 @@ for line in lines[starting_line:]:
     if line.find("state =")!=-1:
         state = int(clear_line(line).split(" ")[-1].strip())
         old_state_name = states.loc[states.index == state, language]
-    # elif line.find("set_state_name")!=-1:
-    #     new_state_name = get_name(line)
-    #     if old_state_name.empty:
-    #         res = input("creating a new state {} called {}?".format(state, new_state_name))
-    #         if (res=='y'):
-    #             new_row = pd.Series({language:new_state_name}, name=state)
-    #             states = states.append(new_row)
-    #             old_state_name = new_state_name
-    #     else:
-    #         old_state_name = old_state_name.item()
-    #     if (old_state_name!=new_state_name):
-    #         res = input("{}: renaming {} to {} in {}?".format(state, old_state_name, new_state_name, language))
-    #         if (res=='y'):
-    #             states.loc[states.index==state,language] = new_state_name
-    #             states.to_csv("new_states.csv")
-    #         elif res!='n':
-    #             states.loc[states.index==state,language] = res
-    #             states.to_csv("new_states.csv")
-    #         else:
-    #              new_state_name = old_state_name
+    elif line.find("set_state_name")!=-1:
+        new_state_name = get_name(line)
+        if old_state_name.empty:
+            res = input("creating a new state {} called {}?".format(state, new_state_name))
+            if (res=='y'):
+                new_row = pd.Series({language:new_state_name}, name=state)
+                states = states.append(new_row)
+                old_state_name = new_state_name
+        else:
+            old_state_name = old_state_name.item()
+        if (old_state_name!=new_state_name):
+            res = input("state {}: renaming {} to {} in {}?".format(state, old_state_name, new_state_name, language))
+            if (res=='y'):
+                states.loc[states.index==state,language] = new_state_name
+                states.to_csv("new_states.csv")
+            elif res!='n':
+                states.loc[states.index==state,language] = res
+                states.to_csv("new_states.csv")
+            else:
+                 new_state_name = old_state_name
     elif line.find("set_province_name")!=-1:
         new_province_name = get_name(line)
         province_id = int(clear_line(line).split(" ")[5])
@@ -62,12 +62,12 @@ for line in lines[starting_line:]:
         else:
             old_province_name = old_province_name.item()
         if (old_province_name!=new_province_name):
-            res = input("{}: renaming {} to {} in {}?".format(province_id, old_province_name, new_province_name, language))
+            res = input("city {}: renaming {} to {} in {}?".format(province_id, old_province_name, new_province_name, language))
             if (res=='y'):
                 provinces.loc[provinces.index==province_id,language] = new_province_name
-                provinces.to_csv("new provinces.csv")
+                provinces.to_csv("new_provinces.csv")
             elif res!='n':
                 provinces.loc[provinces.index==province_id,language] = res
-                provinces.to_csv("new provinces.csv")
+                provinces.to_csv("new_provinces.csv")
 
 
