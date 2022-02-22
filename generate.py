1a3bf9227fce80040af2fbf4210a640892b85daa
Kaboto1 <kaboto1@protonmail.com>
2022-02-22 16:06:49 +0100

    Fix Kosovo + simplified generation

--- generate.py
+++ generate.py
@@ -39,26 +39,20 @@ clr_template = """
 province_template = """
 		set_province_name = {{ id = {} name = "{}" }}"""
 
-# TODO handle case where state is same name but province is not
-
 # for each language
 for language in tqdm(languages):
     dest_file = os.path.join(scripted_effects_folder, "endonyms_{}.txt".format(language))
     with open(dest_file, 'w') as file:
-        # for each state not null in the language column
-        non_null_index = states[language].notna()
-        states_not_null = states.loc[non_null_index]
-        states_not_null = states_not_null[language]
         clear_string = "".join([clr_template.format(lang) for lang in languages.difference(language)])
         state_string = ""
-        for index, state in states_not_null.items():
+        for index, state_row in states.iterrows():
+            translated_name = state_row[language]
+            # get all the provinces of that state
             state_provices = provinces[provinces['StateID'] == index]
-            # select column
-            state_provices = state_provices[[language]]
-            # select rows
+            # null removal
             state_provices = state_provices[state_provices[language].notna()]
             province_string = ""
-            for provinceID, row in state_provices.iterrows():
-                province_string += province_template.format(provinceID, row[0])
-            state_string += state_template.format(index, language, state, language, clear_string, province_string)
+            for provinceID, province_row in state_provices.iterrows():
+                province_string += province_template.format(provinceID, province_row[language])
+            state_string += state_template.format(index, language, translated_name, language, clear_string, province_string)
         file.write(root_template.format(language, state_string))
